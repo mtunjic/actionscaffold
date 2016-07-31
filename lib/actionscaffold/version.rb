dfe689dcf8df591f6b01ff88eea6c38732aa9945
Marko Tunjic <marko.tunjic@icloud.com>
2016-07-31 15:36:21 +0200

    Bootstrap 4 index template

--- lib/actionscaffold/version.rb
+++ lib/actionscaffold/version.rb
@@ -1,3 +1,3 @@
 module ActionScaffold
-  VERSION = '0.2.2.pre'
+  VERSION = '0.2.3.pre'
 end
--- lib/generators/scaffold_view/install_generator.rb
+++ lib/generators/scaffold_view/install_generator.rb
@@ -4,15 +4,30 @@
 #  Created by Marko Tunjic on 15/07/16.
 #  Copyright © 2016 Marko Tunjic. All rights reserved.
 #
+# require 'rails/generators/erb'
+# require 'rails/generators/resource_helpers'
+
 module ScaffoldView
    module Generators
     class InstallGenerator < Rails::Generators::Base
+      #include Rails::Generators::ResourceHelpers
+
+      #argument :ui, :type => :string, :default => 'bootstrap'
+      #argument :attributes, type: :array, default: [], banner: "field:type field:type"
+      ui = "bootstrap"
       desc "This generator override default scaffold generator for views."
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../templates/#{ui}", __FILE__)
+
+
       def copy_template_file
-        copy_file "_form.html.erb",
-          "lib/templates/erb/scaffold/_form.html.erb"
+        #puts argument[:ui]
+        %w(index _form).each do |template|
+          copy_file "#{template}.html.erb",
+            "lib/templates/erb/scaffold/#{template}.html.erb"
+        end
       end
+
+
     end
   end
 end
