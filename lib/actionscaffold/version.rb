201bc6030129f9e7bef23641d927268dbd593a52
Marko Tunjic <marko.tunjic@icloud.com>
2016-08-02 17:25:39 +0200

    bug fixes

--- lib/actionscaffold/version.rb
+++ lib/actionscaffold/version.rb
@@ -1,3 +1,3 @@
 module ActionScaffold
-  VERSION = '0.3.0.pre'
+  VERSION = '0.3.1'
 end
--- lib/generators/layout/layout_generator.rb
+++ lib/generators/layout/layout_generator.rb
@@ -4,20 +4,25 @@
 #  Created by Marko Tunjic on 15/07/16.
 #  Copyright © 2016 Marko Tunjic. All rights reserved.
 #
-class LayoutGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module ScaffoldView
+  module Generators
+    class LayoutGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
 
-  argument :ui, :type => :string, :default => "bootstrap"
+      argument :ui, :type => :string, :default => "bootstrap"
 
-  class_option :stylesheet, :type        => :boolean,
-                            :default     => true,
-                            :description => "Include stylesheets"
+      class_option :stylesheet, :type        => :boolean,
+                                :default     => true,
+                                :description => "Include stylesheets"
 
-  def generate_layout
-    if options.stylesheet?
-      copy_file "application.scss", "public/stylesheets/application.scss"
+      def generate_layout
+        if options.stylesheet?
+          copy_file "application.scss", "public/stylesheets/application.scss"
+        end
+        template "layout.html.erb", "app/views/layouts/application.html.erb"
+
+      end
     end
-    template "layout.html.erb", "app/views/layouts/application.html.erb"
 
   end
 end
